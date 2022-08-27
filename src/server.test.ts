5e17221c319417beabf561148f519aef6af8c0da
Davshn <davshn@gmail.com>
2022-08-27 01:18:04 -0500

    feat testing

---
+++ src/server.test.ts
@@ -0,0 +1,13 @@
+import server from './server'
+import request from 'supertest'
+
+describe('Server is running', () => {
+  test('It should respond with a 404 status in /', async () => {
+    const response = await request(server).get('/').send()
+    expect(response.statusCode).toBe(404)
+  })
+  test('It should respond with a 301 status in /documentation', async () => {
+    const response = await request(server).get('/documentation').send()
+    expect(response.statusCode).toBe(301)
+  })
+})
