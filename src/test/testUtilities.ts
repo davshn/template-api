046d3c1e12e6a77585a0329c6077c4aea17876e4
Davshn <davshn@gmail.com>
2022-08-30 17:45:10 -0500

    feat unit testing

--- src/test/testUtilities.ts
+++ src/test/testUtilities.ts
@@ -1,6 +1,8 @@
 import bcrypt from 'bcrypt'
 
 import { models } from '../models'
+import { userToken, userModel, userRoles } from '../types/auth'
+import { signAuthToken } from '../utils/signTokens'
 
 export const registerTestUser = async (): Promise<void> => {
   const salt = await bcrypt.genSalt(10)
@@ -17,6 +19,49 @@ export const registerTestUser = async (): Promise<void> => {
   await models.User.create(newUser)
 }
 
+export const registerAdminUser = async (): Promise<void> => {
+  const salt = await bcrypt.genSalt(10)
+  const newUser = {
+    name: 'Admin',
+    lastname: 'Admin',
+    documentNumber: '1111111111000',
+    documentType: 'CC',
+    email: 'admin@admin.com',
+    password: await bcrypt.hash('Admin123*', salt),
+    phone: '1111111111000'
+  }
+
+  await models.User.create(newUser)
+}
+
 export const destroyTestUser = async (): Promise<void> => {
   await models.User.destroy({ where: { email: 'test@test.com' } })
+}
+
+export const destroyAdminUser = async (): Promise<void> => {
+  await models.User.destroy({ where: { email: 'admin@admin.com' } })
+}
+
+export const signInTestUser = async (): Promise<userToken> => {
+  const user = await models.User.findOne({ where: { email: 'test@test.com' } }) as userModel
+  user.set({ isVerified: true })
+  await user.save()
+
+  const authToken = signAuthToken(user.id, user.email)
+  return {
+    token: authToken,
+    refreshToken: ''
+  }
+}
+
+export const signInAdminUser = async (): Promise<userToken> => {
+  const user = await models.User.findOne({ where: { email: 'admin@admin.com' } }) as userModel
+  user.set({ isVerified: true, role: userRoles.ADMIN })
+  await user.save()
+
+  const authToken = signAuthToken(user.id, user.email)
+  return {
+    token: authToken,
+    refreshToken: ''
+  }
 }
---
+++ src/test/user/info.test.ts
@@ -0,0 +1,28 @@
+import request from 'supertest'
+
+import { registerTestUser, destroyTestUser, signInTestUser } from '../testUtilities'
+import server from '../../server'
+import { userToken } from '../../types/auth'
+
+describe('User should give own user info and prove authentication', () => {
+  let userToken: userToken
+
+  beforeAll(async () => {
+    await registerTestUser()
+    userToken = await signInTestUser()
+  })
+
+  test('It should respond 401 on malformed token', async () => {
+    const response = await request(server).get('/user/').set('Authorization', userToken.token)
+    expect(response.statusCode).toBe(401)
+  })
+
+  test('It should give own user info on correct token', async () => {
+    const response = await request(server).get('/user/').set('Authorization', 'Bearer ' + userToken.token)
+    expect(response.statusCode).toBe(200)
+  })
+
+  afterAll(async () => {
+    await destroyTestUser()
+  })
+})
