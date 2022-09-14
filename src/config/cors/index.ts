b6899d79cf08f68e268eb4ac83a378bf1b25fb5a
Davshn <davshn@gmail.com>
2022-09-14 00:03:54 -0500

    gcp configuration

--- src/config/cors/index.ts
+++ src/config/cors/index.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from 'cors'
 
 import Logger from '../logger/winston'
 
-const whitelist: string[] = ['https://adoptapi.herokuapp.com', 'http://localhost:4000']
+const whitelist: string[] = ['https://backtemplate.rj.r.appspot.com', 'http://localhost:4000']
 
 const corsConfig = {
   origin: function (origin: string, callback: Function) {
